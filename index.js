6cad563e52d57422e29ed15a2b65736e0c6da974
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2022-02-16 19:28:14 -0300

    asd

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('<h1>Olá Docker</h1>'))
+app.get('/', (req, res) => res.send('<h1>Olá turma 16!</h1>'))
 app.listen(port, () => console.log(`Aplicação rodando na porta ${port}!`))
