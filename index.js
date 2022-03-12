db1eda7cc2e87a86067075c0cc6ff74b7391c82e
Anderson Alves <contatodeanderson@gmail.com>
2022-03-12 19:47:24 -0300

    Add: heroku.yml

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('<h1>Olá turma 16!</h1>'))
 app.listen(port, () => console.log(`Aplicação rodando na porta ${port}!`))
